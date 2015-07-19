258a7c69ab6522d1c874c520a547084b0973acdd
Travis B. Hartwell <nafai@travishartwell.net>
2015-07-18 21:03:17 -0600

    Add some constants to refer to the elements of the options.

--- nixos-options.el
+++ nixos-options.el
@@ -27,14 +27,34 @@
          (options (json-read-file nixos-options-json-file)))
     (mapcar 'add-name-to-cdr options)))
 
+(defconst nixos-options-name "name")
+(defconst nixos-options-type "type")
+(defconst nixos-options-description "description")
+(defconst nixos-options-default "default")
+(defconst nixos-options-example "example")
+(defconst nixos-options-declarations "declarations")
+
+(defconst nixos-options-long-names
+  '((nixos-options-name . "Name")
+    (nixos-options-type . "Type")
+    (nixos-options-description . "Description")
+    (nixos-options-default . "Default value")
+    (nixos-options-example . "Example value")
+    (nixos-options-declarations . "Declared in")))
+
+(defvar nixos-options-name-indent-amount
+  (+ 3 (apply 'max (mapcar (lambda (long-name) (length (cdr long-name))) nixos-options-long-names)))
+  "Indent by the maximum length, plus a colon, plus two spaces.")
+
 (defun helm-source-nixos-options-search ()
   `((name . "NixOS Options")
     (requires-pattern . 2)
     (candidates . nixos-options)
-    (persistent-action . (lambda (f) (message "Pretty Printed: %s" (pp f))))
-    (action . (("Insert into buffer" . (lambda (f) (insert (cdr (assoc "name" f)))))
+    (follow . 1)
+    (persistent-action . (lambda (f) (message "%s" (pp (cdr (assoc nixos-options-description f))))))
+    (action . (("Insert into buffer" . (lambda (f) (insert (cdr (assoc nixos-options-name f)))))
                ("Pretty print" . (lambda (f) (message "Pretty Printed: %s" (pp f))))
-               ("Display name" . (lambda (f) (message "Name: %s" (cdr (assoc "name" f)))))))))
+               ("Display name" . (lambda (f) (message "Name: %s" (cdr (assoc nixos-options-name f)))))))))
 
 (defun helm-search-nixos-options ()
   (interactive)
