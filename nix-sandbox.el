851442697dd1a37095732feee750686642dd3747
Sven Keidel <svenkeidel@googlemail.com>
2016-02-14 11:09:48 +0100

    Incorporate suggestions for nix-sandbox.el for release on MELPA

--- nix-sandbox.el
+++ nix-sandbox.el
@@ -23,7 +23,7 @@
 (defgroup nix nil
   "customizations for nix"
   :prefix "nix-"
-  :group nil)
+  :group 'external)
 
 (defcustom nix-nixpkgs-path nil
   "Absolute path to a nixpkgs directory.
@@ -35,13 +35,13 @@ e.g. /home/user/.nix-defexpr/channels/unstable/nixpkgs"
                  (directory "Custom path to a nixpkgs distribution")))
 
 (defun nix-create-sandbox-rc (sandbox)
-  "Creates a new rc file that contains the environment for the sandbox."
+  "Create a new rc file containing the environment for the given SANDBOX."
   (let* ((env-str (shell-command-to-string
                    (concat "nix-shell "
                            (or (and nix-nixpkgs-path (concat "-I nixpkgs=" nix-nixpkgs-path))
                                "")
                            " --run 'printenv -0' "
-                           sandbox
+                           (shell-quote-argument sandbox)
                            " 2> /dev/null")))
          (env (->> env-str
                    (s-split "\0")
@@ -55,40 +55,41 @@ e.g. /home/user/.nix-defexpr/channels/unstable/nixpkgs"
     tmp-file))
 
 (defvar nix-sandbox-rc-map (make-hash-table :test 'equal
-                                            :size 10))
+                                            :size 4))
 
 (defun nix-sandbox-rc (sandbox)
-  "Returns the rc file for the given sandbox or creates one."
+  "Return the rc file for the given SANDBOX or create one."
   (if (gethash sandbox nix-sandbox-rc-map)
       (gethash sandbox nix-sandbox-rc-map)
     (puthash sandbox (nix-create-sandbox-rc sandbox) nix-sandbox-rc-map)))
 
 ;;;###autoload
 (defun nix-shell-command (sandbox &rest args)
-  "Assembles a nix-shell command that gets executed in the specified sandbox."
+  "Assemble a command from ARGS that can be executed in the specified SANDBOX."
   (list "sh" "-c" (format "source %s; %s" (nix-sandbox-rc sandbox) (s-join " " args))))
 
 (defun nix-shell-string (sandbox &rest args)
+  "Assemble a command string from ARGS that can be executed in the specifed SANDBOX."
    (combine-and-quote-strings
     (apply 'nix-shell-command sandbox args)))
 
 ;;;###autoload
-(defun nix-compile (sandbox &rest args)
+(defun nix-compile (sandbox &rest command)
+  "Compile a program using the given COMMAND in SANDBOX."
   (interactive "Dsandbox: \nMcommand: ")
-  (compile (apply 'nix-shell-string sandbox args)))
+  (compile (apply 'nix-shell-string sandbox command)))
 
 ;;;###autoload
-(defun nix-shell (sandbox &rest args)
-  "Runs a nix-shell command in the given sandbox and returns its output."
-  (shell-command-to-string (apply 'nix-shell-string sandbox args)))
+(defun nix-shell (sandbox &rest command)
+  "Run a COMMAND in the given SANDBOX and return the output."
+  (shell-command-to-string (apply 'nix-shell-string sandbox command)))
 
-;;;###autoload
 (defvar nix-exec-path-map (make-hash-table :test 'equal
-                                           :size 10))
+                                           :size 4))
 
 ;;;###autoload
 (defun nix-exec-path (sandbox)
-  "Returns the `exec-path' of the given sandbox."
+  "Return the `exec-path' of the given SANDBOX."
 
   (if (gethash sandbox nix-exec-path-map)
       (gethash sandbox nix-exec-path-map)
@@ -97,40 +98,35 @@ e.g. /home/user/.nix-defexpr/channels/unstable/nixpkgs"
              nix-exec-path-map)))
 
 ;;;###autoload
-(defun nix-executable-find (sandbox exe)
-  "Searches for an executable in the given sandbox"
+(defun nix-executable-find (sandbox executable)
+  "Search for an EXECUTABLE in the given SANDBOX."
   (let ((exec-path (nix-exec-path sandbox)))
-    (and exec-path (executable-find exe))))
+    (and exec-path (executable-find executable))))
 
 ;;;###autoload
 (defun nix-find-sandbox (path)
-  "Searches for a Nix sandbox files or directories starting at the
-given path,looking upwards. If the directory contains a `shell.nix'
-file, the path to this file is returned. Otherwise if the directory
-contains a `default.nix' file, the parent directory is returned."
+  "Search for a sandbox starting at PATH traversing upwards the directory tree.
+If the directory contains a `shell.nix' file, the path to this
+file is returned.  Otherwise if the directory contains a
+`default.nix' file, the parent directory is returned."
   (and (file-exists-p path)
-   (let* ((sandbox-directory
-           (map-nil 'expand-file-name
-                    (locate-dominating-file path
-                                            '(lambda (dir) (directory-files dir t ".*\.nix$")))))
-          (shell-nix (and sandbox-directory (concat sandbox-directory "shell.nix"))))
-     (if (and sandbox-directory (file-exists-p shell-nix))
-         shell-nix
-       sandbox-directory))))
-
-(defun map-nil (f x)
-  (if x
-      (funcall f x)
-    nil))
+       (let* ((map-nil (lambda (f x) (if x (funcall f x) nil)))
+              (sandbox-directory
+               (funcall map-nil 'expand-file-name
+                        (locate-dominating-file path
+                          '(lambda (dir) (directory-files dir t ".*\.nix$")))))
+              (shell-nix (and sandbox-directory (concat sandbox-directory "shell.nix"))))
+         (if (and sandbox-directory (file-exists-p shell-nix))
+             shell-nix
+           sandbox-directory))))
 
 ;;;###autoload
 (defun nix-current-sandbox ()
-  "Returns the path of the Nix sandbox that is closest
-to the current working directory."
+  "Return the path of the sandbox that is closest to the current working directory."
   (nix-find-sandbox default-directory))
 
 (defun nix-clear-caches ()
-  "clears the cached information for all sandboxes"
+  "Clear cached information for all sandboxes."
   (interactive)
   (clrhash nix-sandbox-rc-map)
   (clrhash nix-exec-path-map))
